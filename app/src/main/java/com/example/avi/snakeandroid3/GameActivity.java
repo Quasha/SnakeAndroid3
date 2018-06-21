7ad28b5614dd9a0257461a54f60182b662684c88
Quasha <se.szeszko@gmail.com>
2018-06-21 17:28:45 +0200

    Fix play button bug.

--- app/src/main/java/com/example/avi/snakeandroid3/GameActivity.java
+++ app/src/main/java/com/example/avi/snakeandroid3/GameActivity.java
@@ -23,7 +23,7 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
 
         gameEngine = new GameEngine();
         gameEngine.initGame();
--- app/src/main/java/com/example/avi/snakeandroid3/MainActivity.java
+++ app/src/main/java/com/example/avi/snakeandroid3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.avi.snakeandroid3;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bPlay = findViewById(R.id.playButton);
+
+        bPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(gameIntent);
+            }
+        });
     }
 }
